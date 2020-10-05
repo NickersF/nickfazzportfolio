2ca6d575365e98b554359bc3c2eb65626461feab
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2020-10-05 14:07:09 -0700

    Main page completed.

--- js/nickfazz.js
+++ js/nickfazz.js
@@ -18,3 +18,37 @@ function swapNavAlignmentCSSClass() {
         el.classList.replace("justify-content-center", "justify-content-end");
     }
 }
+
+// Image switching events for outlinks
+
+// Image logo varaibles
+const gitHubImg = document.getElementById("gitHubImg");
+const codePenImg = document.getElementById("codePenImg");
+const behanceImg = document.getElementById("behanceImg");
+
+// Image swap events for GitHub logo
+gitHubImg.addEventListener("mouseover", function() {
+    gitHubImg.src = "./img/icon_GitHub_Light_Green.svg";
+});
+
+gitHubImg.addEventListener("mouseout", function() {
+    gitHubImg.src = "./img/icon_GitHub_Grey.svg";
+});
+
+// Image swap events for CodePen logo
+codePenImg.addEventListener("mouseover", function() {
+    codePenImg.src = "./img/icon_CodePen_Light_Green.svg";
+});
+
+codePenImg.addEventListener("mouseout", function() {
+    codePenImg.src = "./img/icon_CodePen_Grey.svg";
+});
+
+// Image swap events for Behance logo
+behanceImg.addEventListener("mouseover", function() {
+    behanceImg.src = "./img/icon_Behance_Light_Green.svg";
+});
+
+behanceImg.addEventListener("mouseout", function() {
+    behanceImg.src = "./img/icon_Behance_Grey.svg"
+});
